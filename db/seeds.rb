26b71af06bdb29e828db515acb4a158091788aa3
cassiano yasumitsu <cassiano@usuarioincomum.com>
2020-07-03 22:30:20 +0900

    style

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Clearing DB'
+Flat.destroy_all
+puts 'OK!'
+puts 'Creating 10 seeds now'
+10.times do
+  flat = Flat.new(
+    name: Faker::Company.name,
+    address: Faker::Address.street_address,
+    description: Faker::Restaurant.type,
+    price_per_night: rand(10..100),
+    number_of_guests: rand(1..4)
+  )
+  flat.save
+end
